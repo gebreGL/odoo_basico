61c03a7c18e03b87abcf525e605baaf2607ec1da
gebreGL <gebremedin@gmail.com>
2022-11-08 12:47:17 +0100

    Initial commit

---
+++ models/informacion.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class informacion(models.Model):
+    _name = 'odoo_basico.informacion'
+    _description = 'Exemplo para información'
+
+    name = fields.Char(string="Título:")
+    descripcion = fields.Text(string="Descripción:")
+    alto_encms = fields.Integer(string="Altura en cms:")
+    ancho_en_cms = fields.Integer(string="Anchura en cms:")
+    longo_en_cms = fields.Integer(string="Longo en cms:")
