d0dc64211e92d8c561607b2fdd8a02c2b5b46b0c
gebreGL <gebremedin@gmail.com>
2022-11-10 11:20:50 +0100

    8.11.22

--- models/informacion.py
+++ models/informacion.py
@@ -9,6 +9,10 @@ class informacion(models.Model):
 
     name = fields.Char(string="Título:")
     descripcion = fields.Text(string="Descripción:")
+    peso = fields.Float(string="Peso en KGs:", default=4.5, digits=(6,2))
+    sexo_traducido = fields.Selection([('Mujer','Muller'), ('Hombre','Home'), ('Otros','Outros')], string="Sexo:")
+    autorizado = fields.Boolean(string="¿Autorizado?", default=True)
     alto_en_cms = fields.Integer(string="Altura en cms:")
     ancho_en_cms = fields.Integer(string="Anchura en cms:")
     longo_en_cms = fields.Integer(string="Longo en cms:")
+
